b06ba6734b917649b0b06a1796a3ad98300f70e0
Mario Gutierrez <mgutierrez@itexico.com>
2019-08-21 10:53:27 -0500

    song service and album detail added

--- src/app/components/album-detail/album-detail.component.ts
+++ src/app/components/album-detail/album-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { UserService } from '@app/services/user.service';
+import { AlbumService } from '@app/services/album.service';
+import { SongService } from '@app/services/song.service';
+import { GLOBAL } from '@app/services/global';
+import { AlbumModalComponent } from '@app/components/common/album-modal/album-modal.component';
 
 @Component({
   selector: 'app-album-detail',
@@ -7,9 +14,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumDetailComponent implements OnInit {
 
-  constructor() { }
+  token: string;
+  identity: object;
+
+  albumData;
+  songList: Array <object> = [];
+  albumImgUrl = `${GLOBAL.url}/get-image-album`;
+  defaultAlbumImg: string = '../../../assets/images/default-album.png';
+
+  constructor(
+    private _userService: UserService,
+    private _albumService: AlbumService,
+    private _songService: SongService,
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+    this.token = this._userService.getToken();
+    this.identity = this._userService.getIdentity();
+    this.getArtistInfo();
+  }
+
+  getArtistInfo() {
+    this.route.params.subscribe(async params => {
+
+      const album = await this._albumService.getAlbum(this.token, params.id).toPromise();
+      this.albumData = album['album'];
+      this.loadAlbumImage(this.albumData);
+
+      const songs = await this._songService.getAlbumList(this.token, params.id).toPromise();
+      this.songList = songs['songsList'];
+    });
+  }
+
+  loadAlbumImage(album) {
+    const validation = (album.image && album.image !== 'null') ? true : false;
+    const imgUrl = `${this.albumImgUrl}/${album.image}`;
+    const img = (validation) ? imgUrl : this.defaultAlbumImg;
+    album.image = img;
+  }
+
+  playSong() {
+
+  }
+
+  updateSong() {
+
+  }
+
+  deleteSong() {
+    
   }
 
 }
---
+++ src/app/services/song.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { GLOBAL } from '@app/services/global';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SongService {
+
+  url: String = GLOBAL.url;
+
+  constructor(private _http: HttpClient) { }
+
+  getAlbumList(token, albumId) {
+    const url = `${this.url}/song-list/${albumId}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.get(url, { headers }).pipe(map(res => res));
+  }
+
+  getSong(token, songId) {
+    const url = `${this.url}/song/${songId}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.get(url, { headers }).pipe(map(res => res));
+  }
+
+  getSongFile(token, songName) {
+    const url = `${this.url}/get-file-song/${songName}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.get(url, { headers }).pipe(map(res => res));
+  }
+
+  postSong(token, song) {
+    const url = `${this.url}/song`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.post(url, song, { headers }).pipe(map(res => res));
+  }
+
+  updateSong(token, song) {
+    const url = `${this.url}/song/${song._id}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.put(url, song, { headers }).pipe(map(res => res));
+  }
+
+  deleteSong(token, songId) {
+    const url = `${this.url}/song/${songId}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    return this._http.delete(url, { headers }).pipe(map(res => res));
+  }
+}
